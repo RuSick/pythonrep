9edf7606846d51510425415deecdb38ecf964a6e
RuSick <66710649+RuSick@users.noreply.github.com>
2021-06-08 22:09:07 +0300

    Add files via upload
    1

---
+++ 1/script.py
@@ -0,0 +1,24 @@
+
+def factorial(n):
+    factorial=1
+    while n > 1:
+        factorial *= n
+        n -= 1
+    return factorial
+
+def fibonacci(n):
+    a, b = 1, 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+
+try:
+    n = (int (input ("write n: ") ) )
+except ValueError:
+    print("wrong nubmer")
+    n=0
+
+result = list(fibonacci(n))
+print("Fibo: ", result)
+result = factorial(n)
+print("Fact: ", result)
